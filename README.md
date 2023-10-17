c6137f579a1b125b3d62135f58b284d2fdd45dc4
Noah Slik <noahslik99@gmail.com>
2023-10-17 15:00:12 +0200

    Some formatting

--- README.md
+++ README.md
@@ -3,14 +3,15 @@
 **⚠️ WORK IN PROGRESS ⚠️**
 
 This opens a very simple server, allowing you to get the details from HLTB (How Long To Beat) of a given game.
->Credit to ckatzorke. https://github.com/ckatzorke/howlongtobeat
+
+> Credit to ckatzorke. https://github.com/ckatzorke/howlongtobeat
 
 ## Run the Server
 
 1. Run `npm install` to install the dependencies.
 2. Run `npm start` to start the server.
 
-🐳 Note: You can also run the server in a Docker container using the included Dockerfile. 
+🐳 Note: You can also run the server in a Docker container using the included Dockerfile.
 
 ## How to use
 
--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const hltb = require('howlongtobeat');
+const express = require("express");
+const hltb = require("howlongtobeat");
 
 const app = express();
 const port = 3000;
 
 const hltbService = new hltb.HowLongToBeatService();
 
-app.get('/:id', (req, res) => {
-    hltbService.detail(req.params.id)
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((e) => {
-            console.error(e.message);
-            res.sendStatus(404);
-        });
-})
+app.get("/:id", (req, res) => {
+  hltbService
+    .detail(req.params.id)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((e) => {
+      console.error(e.message);
+      res.sendStatus(404);
+    });
+});
 
 app.listen(port, () => {
-    console.log(`App started on http://localhost:${port}/`);
-})
+  console.log(`App started on http://localhost:${port}/`);
+});
